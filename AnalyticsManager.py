3005ba5383ec76cec69316e18495f0360aaa86f9
denNorske <dennorske@users.noreply.github.com>
2024-07-05 10:21:06 +0200

    Remove unnecessary headers

--- AnalyticsManager.py
+++ AnalyticsManager.py
@@ -27,8 +27,7 @@ class AnalyticsManager:
             {
                 "User-Agent": "Python-requests/{0}".format(requests.__version__),
                 "Accept-Encoding": "gzip, deflate",
-                "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-                "Accept-Language": "en-US,en;q=0.9",
+                "Accept": "text/html,application/xhtml+xml,application/xml",
             }
         )
         self.api_token = self.__get_api_token(username, password)
